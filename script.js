b83829b340ef12a0ece54864782825801a688ee1
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-09-28 19:39:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-// Show a message in the browser console
-console.log("Hello! JavaScript is connected ✅");
+// Dark Mode Toggle
+const darkModeBtn = document.getElementById("darkModeBtn");
 
-// Example: Popup message when page loads
-window.onload = function() {
-  alert("Welcome to my Portfolio Website 🚀");
-};
+// Check saved mode in localStorage
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-mode");
+  darkModeBtn.textContent = "☀️ Light Mode";
+}
+
+darkModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    darkModeBtn.textContent = "☀️ Light Mode";
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    darkModeBtn.textContent = "🌙 Dark Mode";
+    localStorage.setItem("darkMode", "disabled");
+  }
+});
